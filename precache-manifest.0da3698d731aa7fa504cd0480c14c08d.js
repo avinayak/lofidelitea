20b51b4908aa3c36b729ecac17eaaaf45adf5f38
Atul Vinayak <atul@tablecheck.com>
2020-04-09 22:27:12 +0900

    Updates

---
+++ precache-manifest.0da3698d731aa7fa504cd0480c14c08d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "de5896a1c2a9859847b18d44f91c0245",
+    "url": "/lofidelitea/index.html"
+  },
+  {
+    "revision": "bd1641be0a3a8eca53af",
+    "url": "/lofidelitea/static/css/main.c7bddef3.chunk.css"
+  },
+  {
+    "revision": "2739cbf561e7a709391f",
+    "url": "/lofidelitea/static/js/2.1e393888.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/lofidelitea/static/js/2.1e393888.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bd1641be0a3a8eca53af",
+    "url": "/lofidelitea/static/js/main.e1ad9aa8.chunk.js"
+  },
+  {
+    "revision": "08ece3ee14c3e936f873",
+    "url": "/lofidelitea/static/js/runtime-main.6bff696a.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/lofidelitea/static/media/logo.5d5d9eef.svg"
+  }
+]);
