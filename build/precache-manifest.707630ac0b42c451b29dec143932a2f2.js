a62c2a00e92dd155f8bedde34aa8c783febe4b4f
Atul Vinayak <atul@tablecheck.com>
2020-04-09 20:18:41 +0900

    First comit

---
+++ build/precache-manifest.707630ac0b42c451b29dec143932a2f2.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "871e00692c1fca35930cd4f62d72ec2e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "cf328602dd52cdce9ac7",
+    "url": "/static/css/main.c7bddef3.chunk.css"
+  },
+  {
+    "revision": "2739cbf561e7a709391f",
+    "url": "/static/js/2.1e393888.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.1e393888.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cf328602dd52cdce9ac7",
+    "url": "/static/js/main.9ee9fa43.chunk.js"
+  },
+  {
+    "revision": "6143e751519a976d35f7",
+    "url": "/static/js/runtime-main.f7ac63d2.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import LoFi from './LoFi';
+
+function App() {
+  return (
+    <div className="App">
+      <LoFi/>
+    </div>
+  );
+}
+
+export default App;
